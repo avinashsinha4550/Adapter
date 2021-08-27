a140299e12e6678988c684c02bf421c6b8ac8438
avinashsinha4550 <avinash22sinha@gmail.com>
2021-08-27 18:36:18 +0530

    initiate

--- app/src/main/java/com/example/mynewlistview/Register.java
+++ app/src/main/java/com/example/mynewlistview/Register.java
@@ -2,13 +2,35 @@ package com.example.mynewlistview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Register extends AppCompatActivity {
-
+    Button login_register,Register_register;
+    EditText username_register,password_register;
+    SharedPreferences  sharedpreferance;
+    SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        username_register=(EditText) findViewById(R.id.register_edittext);
+        password_register=(EditText) findViewById(R.id.username_edit_register);
+        Register_register=(Button) findViewById(R.id.register_button);
+        Register_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uuser_name=username_register.getText().toString();
+                String passsword_register=password_register.getText().toString();
+                sharedpreferance=getSharedPreferences("USERINFO",0);
+
+                editor.putString("username",uuser_name);
+                editor.putString("Password",passsword_register);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mynewlistview/Shareprefernceexample.java
+++ app/src/main/java/com/example/mynewlistview/Shareprefernceexample.java
@@ -1,14 +1,54 @@
 package com.example.mynewlistview;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Shareprefernceexample extends AppCompatActivity {
+    Button login,Register;
+    EditText username,password;
+    SharedPreferences sharedpreferance;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sharedpreferenceexample);
+        login=(Button) findViewById(R.id.button2);
+        Register=(Button)findViewById(R.id.button3);
+        username=(EditText)findViewById(R.id.editTextTextPersonName3);
+        password=(EditText)findViewById(R.id.editTextTextPersonName4);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Username=username.getText().toString();
+                String Password=password.getText().toString();
+             String usssernnname= sharedpreferance.getString("username","");
+              String paassss= sharedpreferance.getString("Password","");
+              if (Username.equals(usssernnname)&& Password.equals(paassss)){
+                    Intent intent=new Intent(Shareprefernceexample.this,HomeActivity.class);
+                    startActivity(intent);
+              } else{
+                  Toast.makeText(Shareprefernceexample.this, "error", Toast.LENGTH_SHORT).show();
+              }
+
+            }
+        });
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Shareprefernceexample.this,Register.class );
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
